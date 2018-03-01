d141a5be93ab4037608bc0089ef9e0571812677e
mcarmenjc <matrimc@gmail.com>
2018-03-01 18:19:20 +0000

    read file. Use python run.py < <file_name>

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 
-if __name__ == "__main__":
-    print("Hello")
+rows, columns, vehicles, num_rides, bonus, steps = [int(s) for s in input().split(" ")]
+rides = []
+for i in range(0, num_rides):
+  start_x, start_y, end_x, end_y, early_start, latest_end = [int(s) for s in input().split(" ")]
+  rides.append({
+      'start': [start_x, start_y],
+      'end': [end_x, end_y],
+      'early_start': early_start,
+      'latest_end': latest_end
+  })
+  
